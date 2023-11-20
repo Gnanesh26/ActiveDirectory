7e04dc71f538f2391cf849fa4e2ce7aeb575645e
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-11-20 14:57:31 +0530

    initial commit

--- demo/src/main/java/ActiveDirectory/demo/Controller/HealthCheckController.java
+++ demo/src/main/java/ActiveDirectory/demo/Controller/HealthCheckController.java
@@ -1,13 +1,18 @@
 package ActiveDirectory.demo.Controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+
     @GetMapping("/human")
+//    @PreAuthorize("hasAuthority('api://aea554f2-1795-482a-a62d-d6d5bc530b2c/Files.Read')")
+//    @PreAuthorize("hasAuthority('Files.Read')")
+//    @PreAuthorize("hasAuthority('Admin')")
     public String hello() {
-        return "H!, Human how are you ? ? ?";
+        return "Hi, Human! How are you?";
     }
 }
 
---
+++ demo/src/main/java/ActiveDirectory/demo/Security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package ActiveDirectory.demo.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Autowired
+    JwtAuthConverter jwtAuthConverter;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .cors()
+                .and()
+                .authorizeHttpRequests().anyRequest().authenticated()
+                .and().oauth2ResourceServer(t -> t.jwt(Configurer -> Configurer.jwtAuthenticationConverter(jwtAuthConverter))).build();//with defined jwtAuthenticateConverter
+    }
+
+    @Bean
+    public DefaultMethodSecurityExpressionHandler msecurity() {
+        DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler = new DefaultMethodSecurityExpressionHandler();
+        defaultMethodSecurityExpressionHandler.setDefaultRolePrefix("");
+        return defaultMethodSecurityExpressionHandler;
+    }
+}
+
